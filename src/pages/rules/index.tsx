df5f6aa80187866daa732e5c25fca7329b7b9ccf
CasimirColas <casimir.colas@gmail.com>
2024-03-16 23:05:28 +0100

    Add responsive behavior to Rules page

--- src/pages/rules/index.tsx
+++ src/pages/rules/index.tsx
@@ -1,16 +1,17 @@
 import DefaultLayout from "@/components/layout/DefaultLayout";
 import DesktopView from "@/components/pages/rules/DesktopView";
 import MobileView from "@/components/pages/rules/MobileView";
+import useMediaQuery from "@/utils/hooks/useMediaQuery";
 import getStaticPropsI18n from "@/utils/next-intl/getStaticPropsI18n";
 import { GetStaticPropsContext } from "next";
 import { useTranslations } from "next-intl";
 
 export default function Rules() {
   const c = useTranslations("common");
+  const isLg = useMediaQuery("lg");
   return (
     <DefaultLayout title={c("routes.rules")}>
-      {/* <MobileView /> */}
-      <DesktopView />
+      {isLg ? <DesktopView /> : <MobileView />}
     </DefaultLayout>
   );
 }
