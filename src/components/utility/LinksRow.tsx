304f66d530383d50633a935e6f0624f68a6939a4
CasimirColas <casimir.colas@gmail.com>
2024-05-03 14:13:50 +0200

    Update social media links styling in LinksRow component with hover annimation

--- src/components/utility/LinksRow.tsx
+++ src/components/utility/LinksRow.tsx
@@ -17,27 +17,31 @@ function LinksRow({ className }: LinksRowProps) {
     <div
       className={cn("flex justify-center items-center gap-2 pb-10", className)}
     >
-      <Link href={urls.youtubeURL} target="_blank" className="">
+      <Link
+        href={urls.youtubeURL}
+        target="_blank"
+        className="hover:scale-105 transition-all duration-300 ease-in-out"
+      >
         <Youtube w="50" />
       </Link>
       <Link
         href={urls.instagramURL}
         target="_blank"
-        className="rounded-md overflow-hidden"
+        className="rounded-md overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out"
       >
         <Instagram w="50" />
       </Link>
       <Link
         href={urls.twitterURL}
         target="_blank"
-        className="rounded-md overflow-hidden"
+        className="rounded-md overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out"
       >
         <TwitterFilled w="45" />
       </Link>
       <Link
         href={urls.tiktokURL}
         target="_blank"
-        className="rounded-md overflow-hidden"
+        className="rounded-md overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out"
       >
         <TikTokFilled w="45" />
       </Link>
