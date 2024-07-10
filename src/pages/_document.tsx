9ea1cae5a99e92f803c634a53cfbcb85b78b5c37
Casimir Colas <casimir.colas@gmail.com>
2024-07-10 11:42:30 +0200

    Added Google tag for ads in head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
+        `}
+        </Script>
+      </Head>
       <body className="relative">
         <Main />
         <NextScript />
