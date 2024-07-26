0a8602c68df40f82f33e512e98831800ba8c2877
Casimir Colas <casimir.colas@gmail.com>
2024-07-26 11:32:30 +0200

    Manual Script input google tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,13 +17,17 @@ export default function Document() {
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
         `}
         </Script> */}
-        <Script id="Google Tag">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KTBQPBG8');`}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KTBQPBG8');
+            `,
+          }}
+        ></script>
       </Head>
       <body className="relative">
         {/* <!-- Google Tag Manager (noscript) --> */}
